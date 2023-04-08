5be1552f9e175e762c12e5b4aa23b2c156c285db
jjanczynska <joaxjan@gmail.com>
2023-04-08 17:15:47 +0000

    Printing the game board

--- run.py
+++ run.py
@@ -12,7 +12,18 @@ def check_if_space_free(board, position):
     return board[position]==""
 
 # print the current board
-def print_board():
+def print_board(board):
+    print(" |  |")
+    print(""+ board[1]+" | "+ board[2]+" | "+ board[3])
+    print(" |  |")
+    print("------------")
+    print(" |  |")
+    print(""+ board[4]+" | "+ board[5]+" | "+ board[6])
+    print(" |  |")
+    print("------------")
+    print(" |  |")
+    print(""+ board[7]+" | "+ board[8]+" | "+ board[9])
+    print(" |  |")
 
 # check if the player or the computer has won
 def is_winner():
