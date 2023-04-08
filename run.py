1eb68e978e6d08f6d9ebf4478fbff29ae2dd2413
jjanczynska <joaxjan@gmail.com>
2023-04-08 16:46:08 +0000

    Plan out the logic behind the game

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# create an empty board to play the game
+# place a letter on the board
+# check if the space on the board is free
+# print the current board
+# check if the player or the computer has won
+# get the players nove
+# get the computers move
+# check if the board is full
+
+
