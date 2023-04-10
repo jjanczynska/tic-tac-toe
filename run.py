977c7dfb411e73e0745ed1d96c9d889d111f533b
jjanczynska <joaxjan@gmail.com>
2023-04-10 14:17:24 +0000

    Debug the main game code- rewrite so its not as long and pass missing parameters into the functions used

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # create an empty board to play the game
 def create_board():
-    board = [" " for i in range(10)]
+    board = [' ' for i in range(10)]
     return board
 
 
@@ -11,22 +11,22 @@ def insert_letter(board, letter, position):
 
 # check if the space on the board is free
 def is_space_free(board, position):
-    return board[position] == " "
+    return board[position] == ' '
 
 
 # print the current board
 def print_board(board):
-    print("   |  |")
-    print("  " + board[1]+" | " + board[2]+" | " + board[3])
-    print("   |  |")
-    print("------------")
-    print("   |  |")
-    print("  " + board[4]+" | " + board[5]+" | " + board[6])
-    print("   |  |")
-    print("------------")
-    print("   |  |")
-    print("  " + board[7]+" | " + board[8]+" | " + board[9])
-    print("   |  |")
+    print('   |  |')
+    print('  ' + board[1]+' | ' + board[2]+' | ' + board[3])
+    print('   |  |')
+    print('------------')
+    print('   |  |')
+    print('  ' + board[4]+' | ' + board[5]+' | ' + board[6])
+    print('   |  |')
+    print('------------')
+    print('   |  |')
+    print('  ' + board[7]+' | ' + board[8]+' | ' + board[9])
+    print('   |  |')
 
 
 # check if the player or the computer has won
@@ -48,13 +48,13 @@ def get_player_position(board):
         try:
             position = int(position)
             if position < 1  or position > 9:
-                print("Please enter a number between 1 and 9")
+                print('Please enter a number between 1 and 9')
             elif not is_space_free(board, position):
-                print("Space is occupied!")
+                print('Space is occupied!')
             else:
                 return position
         except ValueError:
-            print("Please type a number!")
+            print('Please type a number!')
 
 
 # get the computers move
@@ -65,56 +65,55 @@ def get_computer_move(board, computer_letter):
             insert_letter(board_copy, computer_letter, i)
             if is_winner(board_copy, computer_letter):
                 return i
-
-    for i in range(1, 10):
-        board_copy = board.copy()
         if is_space_free(board_copy, i):
-            insert_letter(board_copy, "X" if computer_letter == "O" else "O", i)
-            if is_winner(board_copy, "X" if computer_letter == "O" else "O"):
+            insert_letter(board_copy, 'XO'[computer_letter == 'O'], i)
+            if is_winner(board_copy, 'XO'[computer_letter == 'O']):
                 return i
-
-    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    for i in range(1, 10):
         if is_space_free(board, i):
             return i
 
 
 # check if the board is full
 def is_board_full(board):
-    return not any([space == "" for space in board])
+    return not any([space == ' ' for space in board])
 
 
 def main_game():
-    print("Tic-Tac-Toe - Welcome to the game!")
+    board = create_board()
+    print('Tic-Tac-Toe - Welcome to the game!')
     print_board(board)
 
     while not (is_board_full(board)):
-        if not (is_winner(board, "O")):
-            get_player_position()
+        if not (is_winner(board, 'O')):
+            position = get_player_position(board)
+            insert_letter(board, 'X', position)
             print_board(board)
         else:
-            print("Sorry, your opponent won this game!")
+            print('Sorry, your opponent won this game!')
             break
 
-        if not(is_winner(board, "X")):
-            position = get_computer_move()
+        if not (is_winner(board, 'X')):
+            position = get_computer_move(board, 'O')
             if position == 0:
-                print("This game is a Tie!")
+                print('This game is a Tie!')
             else:
-                insert_letter("O", position)
-                print("Computer placed an \'O\' in position", position, ":")
+                insert_letter(board, 'O', position)
+                print("Computer placed an 'O' in position:", position)
                 print_board(board)
         else:
-            print("X\ won the game!")
+            print('"X" won the game!')
             break
 
-        if is_board_full(board):
-            print("This game is a Tie!")
+        if is_board_full(board) and not (is_winner(board, 'X')) \
+                and not (is_winner(board, 'O')):
+            print('This game is a Tie!')
 
     while True:
-        answer = input("Would you like to play another round? (Y/N)")
-        if answer.lower() == "y" or answer.lower == "yes":
-            board = ["" for i in range(10)]
-            print("--------------------------------------")
+        answer = input('Would you like to play another round? (Y/N)')
+        if answer.lower() == 'y' or answer.lower() == 'yes':
+            board = create_board()
+            print('--------------------------------------')
             main_game()
         else:
             break
